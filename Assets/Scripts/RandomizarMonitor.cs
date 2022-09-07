45cb5edd05e86ea4fb6a2db3c9f3e9dac0987491
juanma rivero <110467078+Juanm4R@users.noreply.github.com>
2022-09-07 19:46:08 -0300

    randomizar computadoras

---
+++ Assets/Scripts/RandomizarMonitor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizarMonitor : MonoBehaviour
+{
+    [SerializeField] int randomNumber;
+    [SerializeField] GameObject[] Monitores;
+    int monitoresEliminados;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Monitores = GameObject.FindGameObjectsWithTag("Monitor");
+        while (monitoresEliminados != 4)
+        {
+            GenerarNroAleatorio();
+            Monitores[randomNumber].SetActive(false);
+            monitoresEliminados++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerarNroAleatorio()
+    {
+        randomNumber = Random.Range(0, 17);
+    }
+}
